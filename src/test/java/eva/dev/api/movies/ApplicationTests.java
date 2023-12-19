80c20bc04972327652118b084be34dd5be4d1125
Eva López <evamlopez1981@gmail.com>
2023-12-19 09:19:25 +0100

    first commit

---
+++ src/test/java/eva/dev/api/movies/ApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.dev.api.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
